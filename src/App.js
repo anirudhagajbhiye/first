3e67e0dc6199cd69780a673a5a70d88c03c1fbca
anirudhagajbhiye <anirudhagajbhiye2@gmail.com>
2024-06-26 21:06:34 +0530

    Show and Hide Password Using React.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let [status, setStatus] = useState(false);
+  let [mstatus, setmStatus] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={`loginModel ${mstatus === true ? "show" : ""}`}>
+        <span onClick={() => setmStatus(!mstatus)}>&times;</span>
+      </div>
+
+      <div className="inputOuter">
+        <button onClick={() => setmStatus(!mstatus)}>Show Model</button>
+        <h2>Show | Hide Password</h2>
+        <input type={status ? "text" : "password"} />
+        <button onClick={() => setStatus(!status)}>
+          {status ? "Hide" : "Show"}
+        </button>
+      </div>
     </div>
   );
 }
