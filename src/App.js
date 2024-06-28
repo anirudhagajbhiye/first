e2e4d704b284d47fd1f596af9fc2590bb3d4bfb4
anirudhagajbhiye <anirudhagajbhiye2@gmail.com>
2024-06-28 16:23:44 +0530

    FAQs Open close  logic

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { useState } from "react";
+import { question } from "./Data/Question";
 
 function App() {
   let [status, setStatus] = useState(false);
   let [mstatus, setmStatus] = useState(false);
+  let [currentId, setCurrentId] = useState(question[0].id);
+  let allQuestion = question;
 
   return (
     <div className="App">
@@ -20,6 +23,25 @@ function App() {
           {status ? "Hide" : "Show"}
         </button>
       </div>
+
+      <div className="faq"></div>
+      <h1>FAQ</h1>
+      {allQuestion.map((v, i) => {
+        return (
+          <div className="faqItems">
+            <h3
+              onClick={() => {
+                setCurrentId(v.id === currentId ? 0 : v.id);
+              }}
+            >
+              {v.question}
+
+              <span> {currentId === v.id ? "-" : "+"}</span>
+            </h3>
+            <p className={currentId === v.id ? "showP" : " "}>{v.answer}</p>
+          </div>
+        );
+      })}
     </div>
   );
 }
---
+++ src/Data/Question.jsx
@@ -0,0 +1,29 @@
+export let question = [
+  {
+    id: 1,
+    question: " What are the features of React?",
+    answer:
+      "JSX: JSX is a syntax extension to JavaScript. It is used with React to describe what the user interface should look like. By using JSX, we can write HTML structures in the same file that contains JavaScript code.",
+  },
+
+  {
+    id: 2,
+    question: "What is JSX?",
+    answer:
+      "JSX is a syntax extension of JavaScript. It is used with React to describe what the user interface should look like. By using JSX, we can write HTML structures in the same file that contains JavaScript code.",
+  },
+
+  {
+    id: 3,
+    question: "What is the virtual DOM?",
+    answer:
+      "DOM stands for Document Object Model. The DOM represents an HTML document with a logical tree structure. Each branch of the tree ends in a node, and each node contains objects.",
+  },
+
+  {
+    id: 4,
+    question: "Why use React instead of other frameworks, like Angular?",
+    answer:
+      "Easy creation of dynamic applications React makes it easier to create dynamic web applications because it provides less coding and provides more functionality, whereas, with JavaScript applications, code tends to get complex very quickly improved performance React uses virtual DOM which makes web applications perform faster Virtual DOM compares its previous state and updates only those components in the real DOM whose states have changed, rather than updating all the components  like conventional web applications.",
+  },
+];
